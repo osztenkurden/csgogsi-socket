8645b52c8f8c5117811e9d0a34f794e83fca872e
Hubert Walczak <hubert.walczak.lexogrine@gmail.com>
2019-10-24 17:28:39 +0200

    publicized the socket

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,7 @@
+/// <reference types="socket.io-client" />
 import CSGOGSI from 'csgogsi';
-export default function gsisocket(ip: string, eventName: string): CSGOGSI;
+export default function gsisocket(ip: string, eventName: string): {
+    GSI: CSGOGSI;
+    socket: SocketIOClient.Socket;
+};
 export * from 'csgogsi';
