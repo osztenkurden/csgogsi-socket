71bd3a10d8470737ef6e076ea726b695f22e610e
Hubert Walczak <hubertwalczak8@gmail.com>
2021-02-12 18:29:21 +0100

    Added assertion and test

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,10 +1,66 @@
+import CSGOGSI from 'csgogsi';
 import { Socket } from 'socket.io-client';
 import GSISocket from './../tsc';
 
-test("creates socket", () => {
+test("constructor > creates socket", () => {
     const { GSI, socket } = GSISocket("http://localhost:1339/", "update");
 
     socket?.close();
 
     expect(socket).toBeInstanceOf(Socket);
+});
+
+test("constructor > creates CSGOGSI object", () => {
+    const { GSI, socket } = GSISocket("http://localhost:1339/", "update");
+
+    socket?.close();
+
+    expect(GSI).toBeInstanceOf(CSGOGSI);
+});
+
+test("constructor > throws on missing address", () => {
+    const createWIthNoAddress = () => {
+        GSISocket('', 'update');
+    }
+    expect(createWIthNoAddress).toThrow();
+});
+
+test("constructor > throws on non-string address #1", () => {
+    const createWIthNoAddress = () => {
+        const address: any = 123;
+        GSISocket(address, 'update');
+    }
+    expect(createWIthNoAddress).toThrow();
+});
+
+test("constructor > throws on non-string address #2", () => {
+    const createWIthNoAddress = () => {
+        const address: any = [];
+        GSISocket(address, 'update');
+    }
+    expect(createWIthNoAddress).toThrow();
+});
+
+test("constructor > throws on non-string address #3", () => {
+    const createWIthNoAddress = () => {
+        const address: any = {};
+        GSISocket(address, 'update');
+    }
+    expect(createWIthNoAddress).toThrow();
+});
+
+test("constructor > throws on non-string address #4", () => {
+    const createWIthNoAddress = () => {
+        const address: any = null;
+        GSISocket(address, 'update');
+    }
+    expect(createWIthNoAddress).toThrow();
+});
+
+test("constructor > throws on non-string address #5", () => {
+    const createWIthNoAddress = () => {
+        const address: any = undefined;
+        GSISocket(address, 'update');
+    }
+    expect(createWIthNoAddress).toThrow();
 });
--- tsc/index.ts
+++ tsc/index.ts
@@ -1,9 +1,13 @@
 import * as io from 'socket.io-client';
 import CSGOGSI, { CSGORaw } from 'csgogsi';
+import assert from 'assert';
 
-export default function gsisocket(ip: string, eventName: string): { GSI: CSGOGSI, socket: SocketIOClient.Socket } {
+export default function gsisocket(address: string, eventName: string): { GSI: CSGOGSI, socket: SocketIOClient.Socket } {
+    assert(address && typeof address === "string");
+    assert(eventName && typeof eventName === "string");
+
     const GSI = new CSGOGSI();
-    const socket = io.connect(ip);
+    const socket = io.connect(address);
     socket.on(eventName, (data: CSGORaw) => {
         GSI.digest(data);
     });
