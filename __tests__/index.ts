5f608de0f4e98b35d20502c2b85c4ead22388872
Hubert Walczak <hubertwalczak8@gmail.com>
2021-02-14 20:15:11 +0100

    Fixed tests

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,13 +1,66 @@
 import CSGOGSI from 'csgogsi';
-import { Socket } from 'socket.io-client';
+import { Server } from 'socket.io';
+import http from 'http';
 import GSISocket from './../tsc';
+import { createGSIPacket } from './data';
+
+let io: Server;
+let httpServer: http.Server;
+
+const wait = (ms: number) => new Promise(r => setTimeout(r, ms));
+
+beforeAll(done => {
+	httpServer = http.createServer();
+	io = new Server(httpServer);
+
+	httpServer.listen(3000, done);
+});
+
+afterAll(done => {
+	io.close();
+	httpServer.close();
+	done();
+});
 
 test('constructor > creates socket', () => {
 	const { socket } = GSISocket('http://localhost:1339/', 'update');
 
 	socket?.close();
+	expect(socket).toBeDefined();
+});
 
-	expect(socket).toBeInstanceOf(Socket);
+test('constructor > connects to socket.io', async () => {
+	const callback = jest.fn(() => {});
+	const { socket } = GSISocket('http://localhost:3000/', 'update');
+
+	socket.on('connect', callback);
+
+	await wait(50);
+
+	expect(callback.mock.calls.length).toBe(1);
+	expect(socket.connected).toBe(true);
+
+	socket.disconnect();
+});
+
+test('constructor > gets data incoming', async () => {
+	const GSICallback = jest.fn(() => {});
+	const callback = () => {
+		io.emit('update', createGSIPacket());
+	};
+	const { GSI, socket } = GSISocket('http://localhost:3000/', 'update');
+
+	GSI.on('data', GSICallback);
+
+	socket.on('connect', callback);
+
+	// Hubercik kocha Beatkę <3 <3 <3;
+
+	await wait(50);
+
+	expect(GSICallback.mock.calls.length).toBe(1);
+
+	socket.disconnect();
 });
 
 test('constructor > creates CSGOGSI object', () => {
