7e9611297dcad7790072092e15e51ce86d4918bc
Hubert Walczak <hubertwalczak8@gmail.com>
2021-02-22 19:27:44 +0100

    Added esm support

---
+++ lib/cjs/index.d.ts
@@ -0,0 +1,38 @@
+import { CSGOGSI } from 'csgogsi';
+declare const GSISocket: (
+	address: string,
+	eventName: string
+) => {
+	GSI: CSGOGSI;
+	socket: import('socket.io-client').Socket;
+};
+export default GSISocket;
+export {
+	CSGO,
+	CSGORaw,
+	Side,
+	RoundOutcome,
+	WeaponType,
+	WeaponRaw,
+	TeamRaw,
+	PlayerRaw,
+	PlayerObservedRaw,
+	PlayersRaw,
+	Provider,
+	MapRaw,
+	RoundRaw,
+	BombRaw,
+	PhaseRaw,
+	Events,
+	Team,
+	Player,
+	Bomb,
+	Map,
+	Round,
+	Score,
+	KillEvent,
+	RawKill,
+	TeamExtension,
+	PlayerExtension,
+	Orientation
+} from 'csgogsi';
